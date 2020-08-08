e6aaee39acb05bab84adb85dcf914cbcdcfffd00
Ricardo Nascimento <ricardobatistanf@gmail.com>
2020-08-07 21:13:20 -0300

    Integração com Amazon BucketS3

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -11,7 +11,7 @@ import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 const usersRoutes = Router();
 const userController = new UserController();
 const userAvatarController = new UserAvatarController();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 usersRoutes.post(
   '/',
